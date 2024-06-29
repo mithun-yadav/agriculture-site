6683b69c8af7818b5d62e2e181f903288254a635
Mithun <mithuny1998@gmail.com>
2024-06-29 23:31:42 +0530

    fix: formate  code

--- assets/index.js
+++ assets/index.js
@@ -2,14 +2,12 @@ const linkElements = document.querySelectorAll(".nav-list li a");
 
 let navLinkNo = localStorage.getItem("nav-link-no");
 
-if(navLinkNo){
-    let anchorclicked = document.querySelector(
-        `.nav-list li:nth-child(${navLinkNo}) a`
-    );
-    anchorclicked.style.color = "orange";
+if (navLinkNo) {
+  let anchorclicked = document.querySelector(
+    `.nav-list li:nth-child(${navLinkNo}) a`
+  );
+  anchorclicked.style.color = "orange";
 }
-
-
 
 linkElements.forEach((item, index) => {
   item.addEventListener("click", (e) => {
@@ -24,11 +22,11 @@ linkElements.forEach((item, index) => {
 
 const mobileLinkElements = document.querySelectorAll(".menu-items-ul li a");
 
-if(navLinkNo){
-    let MobileLiclicked = document.querySelector(
-        `.menu-items-ul li:nth-child(${navLinkNo})`
-      );
-      MobileLiclicked.style.backgroundColor = "orange";
+if (navLinkNo) {
+  let MobileLiclicked = document.querySelector(
+    `.menu-items-ul li:nth-child(${navLinkNo})`
+  );
+  MobileLiclicked.style.backgroundColor = "orange";
 }
 
 mobileLinkElements.forEach((item, index) => {
