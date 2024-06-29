a185c47235f34bc41447ab2c46bceb848985d7ce
Mithun <mithuny1998@gmail.com>
2024-06-29 16:53:41 +0530

    feat: responsive

---
+++ assets/index.js
@@ -0,0 +1,35 @@
+const linkElements = document.querySelectorAll(".nav-list li a");
+
+let navLinkNo = localStorage.getItem("nav-link-no");
+let anchorclicked = document.querySelector(
+  `.nav-list li:nth-child(${navLinkNo}) a`
+);
+anchorclicked.style.color = "orange";
+
+linkElements.forEach((item, index) => {
+  item.addEventListener("click", (e) => {
+    localStorage.setItem("nav-link-no", index + 1);
+    navLinkNo = localStorage.getItem("nav-link-no");
+    anchorclicked = document.querySelector(
+      `.nav-list li:nth-child(${navLinkNo}) a`
+    );
+    anchorclicked.style.color = "orange";
+  });
+});
+
+const mobileLinkElements = document.querySelectorAll(".menu-items-ul li a");
+let MobileLiclicked = document.querySelector(
+  `.menu-items-ul li:nth-child(${navLinkNo})`
+);
+MobileLiclicked.style.backgroundColor = "orange";
+
+mobileLinkElements.forEach((item, index) => {
+  item.addEventListener("click", (e) => {
+    localStorage.setItem("nav-link-no", index + 1);
+    navLinkNo = localStorage.getItem("nav-link-no");
+    MobileLiclicked = document.querySelector(
+      `.menu-items-ul li:nth-child(${navLinkNo})`
+    );
+    MobileLiclicked.style.backgroundColor = "orange";
+  });
+});
